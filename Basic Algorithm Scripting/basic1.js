028c828f85cd5fe597b8a25395d479ce830db039
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-21 08:08:27 +0000

    create basic folder & first bonfire

---
+++ Basic Algorithm Scripting/basic1.js
@@ -0,0 +1,18 @@
+/* Your goal is to fix the failing test.
+
+First, run all the tests by clicking "Run tests" or by pressing Control + Enter.
+
+The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next Bonfire.
+
+Make this function return true no matter what. */
+
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true; // change 'false' to 'true' and you will pass the tests
+}
+
+meetBonfire("You can do this!");
+
+// The easiest bonfire, but they will get more difficult! Happy coding!
